56b70f39a84b0037b1177ae6e5ce301d458286b1
William Brando Estrada Tepec <westrada@frd.org.mx>
2025-04-22 21:28:08 -0600

    sin nada importante

--- SpeakIAUnitTest/SpeakIAUnitTest.swift
+++ SpeakIAUnitTest/SpeakIAUnitTest.swift
@@ -27,9 +27,7 @@ final class SpeakIAUnitTest: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+
     }
 
 }
